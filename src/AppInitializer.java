a96b8c6334c3a61c3f768ddeeb6dc3311acb844c
M.W.H.Sajitha Chamod Ruwanpura <sajithachamod13579@gmail.com>
2026-01-09 23:49:16 +0530

    stored customer data

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AppInitializer{
     //DataBase Area
     static String [][] users=new String[3][2];
+    static String [][] customers=new String[100][4];
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean exitState=false;
@@ -28,8 +29,8 @@ public class AppInitializer{
             int UserInput = input.nextInt();
             switch (UserInput){
                 case 1:
-                    users[0][0]="sajithachamod13579@gmail.com";
-                    users[0][1]="sajitha123";
+                    users[0][0]="sajitha@gmail.com";
+                    users[0][1]="123";
                     if (login ()){
                     printUi("Dashboard");
                     openDashboard();
@@ -126,16 +127,16 @@ public class AppInitializer{
 
             switch (UserInput){
                 case 1:
-                    printUi("Customer Management");
+                    customerManagement();
                     break ;
                 case 2:
-                    printUi( "Item Management");
+                    itemManagement();
                     break ;
                 case 3:
-                    printUi( "Order Management");
+                    orderManagement();
                     break ;
                 case 4:
-                    printUi( "Logout");
+                    
                     return ;
                 default:
                     System.out.println("Wrong  input");
@@ -144,10 +145,116 @@ public class AppInitializer{
         }
     }
 
+    private static void orderManagement() {
+    }
+
+    private static void itemManagement() {
+    }
+
     //Customer Management process
 public static void customerManagement(){
+        Scanner input = new Scanner(System.in);
+        String customerManagementOptions[]={
+                "1) Save Customer",
+                "2) Find Customers",
+                "3) Update Customer",
+                "4) Delete Customer",
+                "5) Find All Customers",
+                "6) Back to Dashboard"
+        };
+        while (true){
+            for (String option : customerManagementOptions) {
+                System.out.println(option);
 
+            }
+            int UserInput = input.nextInt();
+            switch (UserInput){
+                case 1:
+                   saveCustomer();
+                    break ;
+                case 2:
+                   findCustomer();
+                    break ;
+                case 3:
+                    updateCustomer();
+                    break ;
+                case 4:
+                    deleteCustomer();
+                    break ;
+                case 5:
+                    findAllCustomers();
+                    break ;
+                case 6:
+
+                    return ;
+                default:
+            }
+        }
 }
+    private static void saveCustomer() {
+             //store customer data
+        Scanner input = new Scanner(System.in);
+        while (true){
+            String nic,name,address;
+            double salary;
+            System.out.println("Enter Customer NIC:");
+            nic=input.nextLine();
+            System.out.println("Enter Customer Name:");
+            name=input.nextLine();
+            System.out.println("Enter Customer Address:");
+            address=input.nextLine();
+            System.out.println("Enter Customer Salary:");
+            salary=input.nextDouble();
+
+            customerFoorLoop:
+            for (int i = 0; i < customers.length ; i++) {
+                if(customers[i][0]!=null){
+                    if(customers[i][1].equals(nic)){
+                        System.out.println("Customer already exists");
+                        break;
+                    }
+                    }else {
+                    customers[i][0]=nic;
+                    customers[i][1]=name;
+                    customers[i][2]=address;
+                    customers[i][3]=String.valueOf(salary);
+                    System.out.println("Customer saved successfully!\n");
+                    System.out.println("1)Do you want to add another customer? (yes/no)");
+                    System.out.println("2)Back to Main Menu");
+                    int option=input.nextInt();
+                    switch (option){
+                        case 1:
+                            // Consume newline
+                            break customerFoorLoop;
+                        case 2:
+                            return;
+                        default:
+                            System.out.println("Invalid option. Returning to Main Menu.");
+                            return;
+                    }
+                }
+            }
+        }
+    }
+    private static void findCustomer() {
+
+    }
+    private static void updateCustomer() {
+
+    }
+    private static void deleteCustomer() {
+
+    }
+    private static void findAllCustomers() {
+    }
+
+
+
+
+
+
+
+
 
     public static void printUi(String position){
 
